7434c4a62451f1dfaa3d90c8b3af7c18a22bbc1b
Amir Kasumovic <amir.kasumovic@live.com>
2021-10-19 14:32:02 +0200

    rotate and drag a phone

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,17 @@ import {
 
 function App() {
   const { scrollYProgress } = useViewportScroll();
-  const scale = useTransform(scrollYProgress, [0.65, 0.65, 0.755], [4, 4, 0.6]);
-  // const scale = useTransform(scrollYProgress, [0, -1], [2, 1]);
-  const y = useTransform(scrollYProgress, [0, -0.5, -1.5], [0, 0.5, 0.5]);
-
-  useEffect(() => {
-    console.log(scale);
-  }, [scale, scrollYProgress]);
+  // first value is where it starts from (scroll number)
+  // second value is the same as the first one
+  // third value is where the animation ends
+  // all in all, first array are the positions, and the second one are sizes
+  const scale = useTransform(scrollYProgress, [0.73, 0.73, 0.8], [4, 4, 0.5]); // Values from the second array: 1. size during coresponding value of the first array (position), until the second value
+  const y = useTransform(scrollYProgress, [0.73, 0.73, 0.8], [0, 35, 35]);
+  const rotate = useTransform(
+    scrollYProgress,
+    [0.73, 0.73, 0.8],
+    [180, 180, 360]
+  );
 
   return (
     <div className='App'>
@@ -72,14 +76,15 @@ function App() {
           Learn React
         </a>
       </header>
-      <motion.div className='iPhone' style={{ scale: scale, y: y }}>
-        <img
+      <div className='iPhone'>
+        <motion.img
           src='./iPhone.webp'
           alt=''
-          className='iPhone'
-          style={{ scale: scale, y: y }}
+          style={{ scale: scale, y: y, rotate: 90 }}
+          drag='x'
+          dragConstraints={{ left: -100, right: 100 }}
         />
-      </motion.div>
+      </div>
 
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
